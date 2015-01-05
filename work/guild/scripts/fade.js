2a615c7d848968abaa9f61677cf26bab11301f80
grantford05 <grantford05@gmail.com>
2015-01-06 00:52:55 +1300

    renamed html

--- work/guild/scripts/fade.js
+++ work/guild/scripts/fade.js
@@ -1,3 +1,9 @@
+/*Fade in splash image on page load (5 seconds)*/
 $(function(){ 
 	$('#emblem').hide().fadeIn(5000);
+});
+
+/*Fade out splash and fade in home content on emblem click (3 seconds)*/
+$("#emblem").click(function(){
+  $("#emblem").fadeOut(3000);
 });
