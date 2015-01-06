da9f5cd7de107e06551833cada6c3928602129b6
grantford05 <grantford05@gmail.com>
2015-01-06 22:21:19 +1300

    added horizontal pages

--- work/guild/scripts/fade.js
+++ work/guild/scripts/fade.js
@@ -3,6 +3,11 @@ $(function(){
 	$('#emblem').hide().fadeIn(5000);
 });
 
+/*Scroll to home on click*/
+$(".home").click(function(){
+  $("#emblem").fadeOut(3000);
+});
+
 /*Fade out splash and fade in home content on emblem click (3 seconds)*/
 $("#emblem").click(function(){
   $("#emblem").fadeOut(3000);
